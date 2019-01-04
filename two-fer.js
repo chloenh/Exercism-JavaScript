2bfc69e73abfb093e8d09ee81acb8a76f4b6213d
cnastyah <45791822+cnastyah@users.noreply.github.com>
2019-01-04 21:17:56 +0100

    Add files via upload

---
+++ two-fer.js
@@ -0,0 +1,9 @@
+var TwoFer = function () { };
+
+TwoFer.prototype.twoFer = function (who) {
+    who = who || 'you';
+    return `One for ${who
+        }, one for me.`;
+};
+
+module.exports = TwoFer;
